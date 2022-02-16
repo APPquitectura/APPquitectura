f23a1488b0478d5556762c5903203ad72a123de8
Christian Sandoval <christian.sandoval@optivamedia.com>
2022-02-16 08:26:52 +0100

    Splash animation

--- app/src/main/java/com/etsisi/quizz/presentation/ui/login/view/LoginActivity.kt
+++ app/src/main/java/com/etsisi/quizz/presentation/ui/login/view/LoginActivity.kt
@@ -1,5 +1,12 @@
 package com.etsisi.quizz.presentation.ui.login.view
 
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.os.Build
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.annotation.RequiresApi
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModel
@@ -7,6 +14,7 @@ import com.etsisi.quizz.R
 import com.etsisi.quizz.databinding.ActivityLoginBinding
 import com.etsisi.quizz.presentation.common.BaseActivity
 import com.etsisi.quizz.presentation.ui.login.viewmodel.LoginViewModel
+import com.etsisi.quizz.presentation.utils.deviceApiIsAtLeast
 
 class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(
     R.layout.activity_login, LoginViewModel::class
@@ -19,8 +27,35 @@ class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(
         //TODO("Not yet implemented")
     }
 
+
+    @SuppressLint("NewApi")
     override fun setUpSplashScreen() {
         installSplashScreen()
+        if (deviceApiIsAtLeast(Build.VERSION_CODES.S)) {
+            setUpSplashAnimation()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun setUpSplashAnimation() {
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            // Create your custom animation.
+            val slideUp = ObjectAnimator.ofFloat(
+                splashScreenView,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenView.height.toFloat()
+            )
+            slideUp.interpolator = AnticipateInterpolator()
+            slideUp.duration = 200L
+
+            // Call SplashScreenView.remove at the end of your custom animation.
+            slideUp.doOnEnd { splashScreenView.remove() }
+
+            // Run your animation.
+            slideUp.start()
+        }
+
     }
 
 }
